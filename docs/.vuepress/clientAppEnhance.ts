8401ee7565b40ee6e17b51e44f61302cb3f52bef
jon <support@indigit.pt>
2022-03-15 12:04:14 +0000

    Bugfix Typings

--- docs/.vuepress/clientAppEnhance.ts
+++ docs/.vuepress/clientAppEnhance.ts
@@ -2,13 +2,6 @@ import { defineClientAppEnhance } from '@vuepress/client'
 import { VanillaComponents } from "@indigit/vanilla-components";
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {
-
-  const configuration = {
-    VanillaInput: {
-      placeholder: 'Elon Musky'
-    },
-  };
-
+  const configuration = {};
   app.use(VanillaComponents, configuration)
-
 })
